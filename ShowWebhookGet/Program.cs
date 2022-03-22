d76bd15fa1b61232a525020ec2462d1568ee4d73
poychang <poypost@gmail.com>
2022-03-22 14:25:34 +0800

    init project

---
+++ ShowWebhookGet/Program.cs
@@ -0,0 +1,40 @@
+using ShowWebhookGet;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSingleton<QueueService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseStaticFiles();
+
+// Webhook endpoint
+app.MapPost("api/webhook", (HttpContext context, QueueService queueService) =>
+{
+    using var reader = new StreamReader(context.Request.Body, System.Text.Encoding.UTF8);
+    var content = reader.ReadToEndAsync().GetAwaiter().GetResult();
+    queueService.Push(content.Replace(System.Environment.NewLine, string.Empty));
+});
+// server-sent event
+app.MapGet("api/server-send-events", async (HttpContext context, QueueService queueService) =>
+{
+    context.Response.Headers.Add("Content-Type", "text/event-stream");
+    while (true)
+    {
+        await Task.Delay(3000);
+        var content = queueService.Pop();
+
+        if (!string.IsNullOrEmpty(content))  await context.Response.WriteAsync($"data: {content}\n\n");
+
+        await context.Response.Body.FlushAsync();
+    }
+});
+
+app.Run();
---
+++ ShowWebhookGet/QueueService.cs
@@ -0,0 +1,15 @@
+﻿namespace ShowWebhookGet;
+
+public class QueueService
+{
+    private Queue<string> Queue;
+
+    public QueueService()
+    {
+        Queue = new Queue<string>();
+    }
+
+    public void Push(string content) { Queue.Enqueue(content); }
+
+    public string Pop() => Queue.TryDequeue(out var result) ? result : string.Empty;
+}
